a1fbce2370877008f71ecc3c765d51e02a7fad27
tarek-brahimi <tarekbrahimi98@gmail.com>
2026-03-12 18:18:56 +0100

    config : adding order component to the router making him a page and  config all the links

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/home";
 import "./App.css";
 import { CheckoutPage } from "./pages/checkoutpage";
-
+import { Order  } from "./pages/ordeer.JSX";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="order" element={<Order/>} />
       </Routes>
     </>
   );
