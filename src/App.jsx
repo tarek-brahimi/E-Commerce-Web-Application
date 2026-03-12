e7ac004d5807ea664404aab24cd7a40eb6d1939e
tarek-brahimi <tarekbrahimi98@gmail.com>
2026-03-12 17:27:40 +0100

    config : creating multipel pages with react routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { Routes,Route } from 'react-router'
 import { HomePage } from './pages/home'
 import './App.css'
 
@@ -6,7 +6,11 @@ function App() {
   
   return (
     <>
-    <HomePage/>
+    <Routes>
+      <Route path ="/" element={<HomePage/>}/>
+      <Route path ="checkout" element={<div> TEST</div>}/>
+    </Routes>
+    
     </>
   )
 }
