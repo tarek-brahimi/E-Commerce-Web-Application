9888128b56c7d53f5702ae9bc6695bf9f1101112
tarek-brahimi <tarekbrahimi98@gmail.com>
2026-03-12 17:58:44 +0100

    adding checkout to browser route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { Routes,Route } from 'react-router'
-import { HomePage } from './pages/home'
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/home";
+import "./App.css";
+import { CheckoutPage } from "./pages/checkoutpage";
 
 function App() {
-  
   return (
     <>
-    <Routes>
-      <Route path ="/" element={<HomePage/>}/>
-      <Route path ="checkout" element={<div> TEST</div>}/>
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
