c228b43f1d27929c146ed0ebd100c2f251bfdd10
tarek-brahimi <tarekbrahimi98@gmail.com>
2026-03-12 18:07:52 +0100

    styling : checkout page

--- src/pages/checkoutpage.jsx
+++ src/pages/checkoutpage.jsx
@@ -1,6 +1,10 @@
+import './checkoutPage.css'
+import './checkout-header.css'
+
 export function CheckoutPage() {
   return (
     <>
+    <title>Checkout</title>
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
